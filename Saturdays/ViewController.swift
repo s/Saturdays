7c0d7f4b8ff8c2de8059a98ab46820d0b84fc633
Said Ozcan <said.ozcan@booking.com>
2017-02-11 19:21:26 +0100

    Initial Commit

---
+++ Saturdays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 2/11/17.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
