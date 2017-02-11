936fc84cf9e9285271ca77843eb0d17612848604
Said Ozcan <said.ozcan@booking.com>
2017-02-11 19:24:45 +0100

    update project structure

---
+++ Saturdays/Common/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 2/11/17.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: ***REMOVED***UIApplicationLaunchOptionsKey: Any***REMOVED***?) -> Bool {
+        return true
+    }
+}
+
