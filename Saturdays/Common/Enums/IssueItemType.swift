c969f71ebf2caf0ff800e6e8c37269470b63ca0e
Said Ozcan <said.ozcan@booking.com>
2017-03-03 10:14:31 +0100

    add IssueItemType

---
+++ Saturdays/Common/Enums/IssueItemType.swift
@@ -0,0 +1,15 @@
+//
+//  IssueItemType.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 3/3/17.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+
+enum IssueItemType : String, Equatable {
+    case track = "Track"
+    case venue = "Venue"
+    case post  = "Post"
+}
