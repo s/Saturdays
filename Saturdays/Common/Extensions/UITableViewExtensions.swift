32013418a556721a2c13906ae48dc6b5f284341d
Said Ozcan <said.ozcan@booking.com>
2017-03-03 10:12:24 +0100

    add sizeHeaderToFit method to UITableViewExtensions

--- Saturdays/Common/Extensions/UITableViewExtensions.swift
+++ Saturdays/Common/Extensions/UITableViewExtensions.swift
@@ -15,4 +15,21 @@ extension UITableView{
         let nib = UINib(nibName: T.name, bundle: Bundle.main)
         register(nib, forCellReuseIdentifier: T.name)
     }
+    
+    func sizeHeaderToFit() {
+        guard let headerView = self.tableHeaderView else { return }
+        
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+        let height = headerView.systemLayoutSizeFitting(UILayoutFittingCompressedSize).height
+        
+        let oldFrame = headerView.frame
+        let newFrame = CGRect(x:oldFrame.origin.x ,
+                              y: oldFrame.origin.y,
+                              width: oldFrame.size.width,
+                              height: height)
+        headerView.frame = newFrame
+        
+        self.tableHeaderView = headerView
+    }
 }
