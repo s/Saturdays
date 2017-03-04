b0e3d1534dce5f39e8d39d23ed898489a05c981d
Said Ozcan <said.ozcan@booking.com>
2017-03-05 00:09:51 +0100

    add UITableViewExtensions and ImageDownloadableModel files

---
+++ Saturdays/Common/Protocols/ImageDownloadableModel.swift
@@ -0,0 +1,13 @@
+//
+//  ImageDownloadableModel.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 3/4/17.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+
+protocol ImageDownloadableModel {
+    var imageURL : URL { get }
+}
