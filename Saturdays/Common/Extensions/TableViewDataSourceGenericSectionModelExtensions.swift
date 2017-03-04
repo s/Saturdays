49b7171c4fd6193bd2a611fa8539faec2aab31ff
Said Ozcan <said.ozcan@booking.com>
2017-03-05 00:09:51 +0100

    add UITableViewExtensions and ImageDownloadableModel files

---
+++ Saturdays/Common/Extensions/TableViewDataSourceGenericSectionModelExtensions.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewDataSourceGenericSectionModelExtensions.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 3/4/17.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireImage
+
+extension TableViewDataSourceGenericSectionModel {
+    func download(cellImage url: URL,
+                  downloadProgressHandler: @escaping (Progress) -> Void,
+                  completionHandler:@escaping (DataResponse<Image>) -> Void)
+    {
+        Alamofire.request(url).downloadProgress(closure: { (progress) in
+            downloadProgressHandler(progress)
+        }).responseImage { (response) in
+            completionHandler(response)
+        }
+    }
+}
