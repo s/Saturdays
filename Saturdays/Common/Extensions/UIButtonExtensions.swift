567a4581db81790cc5bb4befc4cb5cd2d0750ce2
Said Ozcan <said.ozcan@booking.com>
2017-03-22 01:13:44 +0100

    add UIButtonExtensions for hitTest

---
+++ Saturdays/Common/Extensions/UIButtonExtensions.swift
@@ -0,0 +1,28 @@
+//
+//  UIButtonExtensions.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 3/22/17.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+fileprivate let minimumHitArea = CGSize(width: 50, height: 50)
+
+extension UIButton {
+    open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        // if the button is hidden/disabled/transparent it can't be hit
+        if self.isHidden || !self.isUserInteractionEnabled || self.alpha < 0.01 { return nil }
+        
+        // increase the hit frame to be at least as big as `minimumHitArea`
+        let buttonSize = self.bounds.size
+        let widthToAdd = max(minimumHitArea.width - buttonSize.width, 0)
+        let heightToAdd = max(minimumHitArea.height - buttonSize.height, 0)
+        let largerFrame = self.bounds.insetBy(dx: -widthToAdd / 2, dy: -heightToAdd / 2)
+        
+        // perform hit test on larger frame
+        return (largerFrame.contains(point)) ? self : nil
+    }
+}
