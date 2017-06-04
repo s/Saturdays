e698b57abaa19e49cbb2e9f4d1f4230fa7d31827
Said Ozcan <said.ozcan@booking.com>
2017-06-04 18:01:06 +0200

    update NotificationService

--- Saturdays/Common/Services/NotificationService.swift
+++ Saturdays/Common/Services/NotificationService.swift
@@ -12,9 +12,15 @@ import Firebase
 import FirebaseInstanceID
 import FirebaseMessaging
 
-class NotificationService : NSObject, UNUserNotificationCenterDelegate, FIRMessagingDelegate {
+protocol NotificationServiceProtocol {
+    func applicationDidBecomeActive(_ application: UIApplication)
+    func applicationDidEnterBackground(_ application: UIApplication)
+}
+
+class NotificationService : NSObject {
     
     fileprivate weak var application:UIApplication?
+    fileprivate let gcmMessageIDKey = "gcm.message_id"
     
     init(with application:UIApplication) {
         self.application = application
@@ -23,27 +29,73 @@ class NotificationService : NSObject, UNUserNotificationCenterDelegate, FIRMessa
     
     func registerForNotifications() {
         guard let application = application else { return }
-        if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            let authOptions: UNAuthorizationOptions = ***REMOVED***.alert, .badge, .sound***REMOVED***
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-            // For iOS 10 data message (sent via FCM
-            FIRMessaging.messaging().remoteMessageDelegate = self
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: ***REMOVED***.alert, .badge, .sound***REMOVED***, categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
         
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = ***REMOVED***.alert, .badge, .sound***REMOVED***
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
         application.registerForRemoteNotifications()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.tokenRefreshNotification),
+                                               name: .firInstanceIDTokenRefresh,
+                                               object: nil)
         
         FIRApp.configure()
     }
     
-    func applicationReceivedRemoteMessage(_ remoteMessage: FIRMessagingRemoteMessage) {
-        print(remoteMessage.appData)
+    @objc fileprivate func tokenRefreshNotification(_ notification: Notification) {
+        if let refreshedToken = FIRInstanceID.instanceID().token() {
+            print("InstanceID token: \(refreshedToken)")
+        }
+        
+        // Connect to FCM since connection may have failed when attempted before having a token.
+        connectToFcm()
+    }
+    
+    fileprivate func connectToFcm() {
+        // Won't connect since there is no token
+        guard FIRInstanceID.instanceID().token() != nil else {
+            return
+        }
+        
+        // Disconnect previous FCM connection if it exists.
+        FIRMessaging.messaging().disconnect()
+        
+        FIRMessaging.messaging().connect { (error) in
+            if error != nil {
+                print("Unable to connect with FCM. \(String(describing: error?.localizedDescription))")
+***REMOVED*** else {
+                print("Connected to FCM.")
+***REMOVED***
+        }
+    }
+}
+
+extension NotificationService : NotificationServiceProtocol {
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        connectToFcm()
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        FIRMessaging.messaging().disconnect()
+        print("Disconnected from FCM.")
+    }
+}
+
+extension NotificationService : UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        // Print message ID.
+        if let messageID = userInfo***REMOVED***gcmMessageIDKey***REMOVED*** {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        completionHandler()
     }
 }
