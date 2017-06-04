068ba1fad75cebd3fbb4022dea85640893536a85
Said Ozcan <said.ozcan@booking.com>
2017-06-04 16:44:11 +0200

    add ViewModelProtocol

---
+++ Saturdays/Common/Protocols/ViewModelProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  ViewModelProtocol.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 03/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewModelProtocol {}
