d8c14b813336613fe2aca844884a59ae82606f27
Said Ozcan <said.ozcan@booking.com>
2017-06-13 01:57:54 +0200

    add Track, Venue and PostViewModels

---
+++ Saturdays/Common/ViewModels/TrackViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  TrackViewModel.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 13/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import UIKit
+
+class TrackViewModel : NSObject, ViewModelProtocol {
+    
+    // MARK : Properties
+    let trackName : String
+    let artistName : String
+    let albumArtUrl : URL
+    let albumArtSize: CGSize?
+    
+    // MARK : Lifecycle
+    init(with track:Track) {
+        self.trackName = track.name
+        self.artistName = track.artist
+        self.albumArtUrl = track.albumArt.url
+        self.albumArtSize = track.albumArt.size
+        super.init()
+    }
+}
---
+++ Saturdays/Common/ViewModels/VenueViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  VenueViewModel.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 13/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import UIKit
+
+class VenueViewModel : NSObject, ViewModelProtocol {
+    
+    // MARK : Properties
+    let venueName : String
+    let type : String
+    let locationInfo : String
+    let photoUrl : URL
+    let photoSize : CGSize?
+    
+    // MARK : Lifecycle
+    init(with venue:Venue) {
+        self.venueName = venue.name
+        self.type = venue.type
+        self.locationInfo = venue.locationInfo
+        self.photoUrl = venue.photo.url
+        self.photoSize = venue.photo.size
+        super.init()
+    }
+}
+
