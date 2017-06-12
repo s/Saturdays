de4933dfa45a13d082eaa92eb797aa9d5345176b
Said Ozcan <said.ozcan@booking.com>
2017-06-12 02:02:19 +0200

    change UI design of IssueListView

--- Saturdays/Screens/IssueList/IssueListView.swift
+++ Saturdays/Screens/IssueList/IssueListView.swift
@@ -16,17 +16,33 @@ protocol IssueListViewProtocol {
     func show(issues:***REMOVED***IssueViewModel***REMOVED***)
 }
 
+fileprivate enum OSVersion : Int {
+    case eleven
+    case ten
+}
+
 class IssueListView : UIViewController {
     
     //MARK: Properties
+    fileprivate let osVersion : OSVersion
     fileprivate let presenter : IssueListPresenter
     fileprivate lazy var tableViewDataSource : IssueListViewDataSource = { ***REMOVED***unowned self***REMOVED*** in
+        
+        let selectHandler : (IssueListViewDataSourceSelection) -> Void = { ***REMOVED***unowned self***REMOVED*** (conf) in
+            self.handleSelection(with:conf)
+        }
+        
         return IssueListViewDataSource(with: self.tableView,
-                                       imageDownloadService:self.imageDownloadService)
+                                       imageDownloadService:self.imageDownloadService,
+                                       selectHandler: selectHandler)
     }()
     fileprivate lazy var imageDownloadService : ImageDownloadService = {
         return ImageDownloadService()
     }()
+    fileprivate lazy var issueDetailsAnimator : IssueDetailsAnimator = {
+        return IssueDetailsAnimator()
+    }()
+    fileprivate var selectedIndexPath : IndexPath?
     
     //MARK: Subviews
     fileprivate let loadingIndicator : UIStackView = {
@@ -40,13 +56,65 @@ class IssueListView : UIViewController {
         tableView.tableFooterView = UIView()
         tableView.separatorStyle = UITableViewCellSeparatorStyle.none
         tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 250.0
+        tableView.estimatedRowHeight = UIDefines.Sizes.defaultIssueCellHeight
         return tableView
+    }()
+    fileprivate lazy var navigationBarView : UIView = { ***REMOVED***unowned self***REMOVED*** in
+        let view = UIView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIDefines.Colors.navigationBarTintColor
+        view.addSubview(self.navigationBarContentView)
+        view.addSubview(self.navigationBarLargeTitleView)
+        view.bringSubview(toFront: self.navigationBarContentView)
+        view.addConstraints(***REMOVED***
+            self.navigationBarContentView.topAnchor.constraint(equalTo:view.topAnchor),
+            self.navigationBarContentView.leadingAnchor.constraint(equalTo:view.leadingAnchor),
+            self.navigationBarContentView.trailingAnchor.constraint(equalTo:view.trailingAnchor),
+            self.navigationBarContentView.heightAnchor.constraint(equalToConstant:UIDefines.Sizes.navigationBarContentViewHeight),
+            
+            self.navigationBarLargeTitleView.topAnchor.constraint(equalTo:self.navigationBarContentView.bottomAnchor),
+            self.navigationBarLargeTitleView.leadingAnchor.constraint(equalTo:view.leadingAnchor),
+            self.navigationBarLargeTitleView.trailingAnchor.constraint(equalTo:view.trailingAnchor),
+            self.navigationBarLargeTitleView.heightAnchor.constraint(equalToConstant:UIDefines.Sizes.navigationBarLargeTitleViewHeight),
+            
+***REMOVED***)
+        return view
+    }()
+    fileprivate lazy var navigationBarLargeTitleView : UIView = { ***REMOVED***unowned self***REMOVED*** in
+        let view = UIView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIDefines.Colors.navigationBarTintColor
+        view.addSubview(self.navigationBarLargeTitleLabel)
+        view.addConstraints(***REMOVED***
+            self.navigationBarLargeTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant:UIDefines.Spacings.doubleUnit),
+            self.navigationBarLargeTitleLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:-UIDefines.Spacings.singleUnit),
+            self.navigationBarLargeTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant:-UIDefines.Spacings.doubleUnit)
+***REMOVED***)
+        return view
+    }()
+    fileprivate lazy var navigationBarContentView : UIView = { ***REMOVED***unowned self***REMOVED*** in
+        let view = UIView(frame: CGRect.zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIDefines.Colors.navigationBarTintColor
+        return view
+    }()
+    fileprivate lazy var navigationBarLargeTitleLabel : UILabel = {
+        let label = UILabel(frame: CGRect.zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIDefines.Fonts.navigationBarLargeTitleViewTitle
+        label.text = UIDefines.Copies.saturdaysTitle
+        label.textColor = UIDefines.Colors.black
+        return label
     }()
     
     //MARK: Lifecycle
     init(with presenter:IssueListPresenter) {
         self.presenter = presenter
+        if #available(iOS 11.0, *) {
+            self.osVersion = .eleven
+        } else {
+            self.osVersion = .ten
+        }
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -57,7 +125,7 @@ class IssueListView : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.title = "SATURDAYS"
+        self.title = UIDefines.Copies.saturdaysTitle
         self.setupTableView()
         self.addSubviews()
         self.setupLayoutConstraints()
@@ -72,23 +140,53 @@ class IssueListView : UIViewController {
     }
     
     fileprivate func addSubviews() {
+            self.view.addSubview(self.tableView)
         self.view.addSubview(self.loadingIndicator)
-        self.view.addSubview(self.tableView)
+        if osVersion == .ten {
+            self.view.addSubview(self.navigationBarView)
+        }
     }
     
     fileprivate func setupLayoutConstraints() {
-        let constraints : ***REMOVED***NSLayoutConstraint***REMOVED*** = ***REMOVED***
-            loadingIndicator.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor),
+        var topAnchor : NSLayoutYAxisAnchor
+        if osVersion == .ten {
+            topAnchor = self.navigationBarView.bottomAnchor
+        } else {
+            topAnchor = self.view.topAnchor
+        }
+        
+        var constraints : ***REMOVED***NSLayoutConstraint***REMOVED*** = ***REMOVED***
+            loadingIndicator.topAnchor.constraint(equalTo: topAnchor),
             loadingIndicator.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             loadingIndicator.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor),
             loadingIndicator.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             
-            tableView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: topAnchor),
             tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             tableView.bottomAnchor.constraint(equalTo: self.bottomLayoutGuide.topAnchor),
             tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
 ***REMOVED***
+        
+        if osVersion == .ten {
+            constraints.append(contentsOf: ***REMOVED***
+                navigationBarView.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor),
+                navigationBarView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                navigationBarView.heightAnchor.constraint(equalToConstant: UIDefines.Sizes.navigationBarViewHeight),
+                navigationBarView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+    ***REMOVED***)
+        }
+        
         NSLayoutConstraint.activate(constraints)
+    }
+    
+    fileprivate func handleSelection(with conf:IssueListViewDataSourceSelection) {
+        self.selectedIndexPath = conf.indexPath
+        
+        let detailView = self.presenter.getDetailView(for: conf)
+        detailView.transitioningDelegate = self
+        DispatchQueue.main.async {
+            self.present(detailView, animated: true, completion: nil)
+        }
     }
 }
 
@@ -99,6 +197,7 @@ extension IssueListView : IssueListViewProtocol {
     }
     
     func hideLoadingIndicator() {
+        self.loadingIndicator.removeFromSuperview()
         self.loadingIndicator.isHidden = true
         self.tableView.isHidden = false
     }
@@ -110,5 +209,24 @@ extension IssueListView : IssueListViewProtocol {
     func show(issues:***REMOVED***IssueViewModel***REMOVED***) {
         self.tableViewDataSource.update(with: issues)
         self.tableView.reloadData()
+    }
+}
+
+extension IssueListView : UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        guard let selectedIndexPath = selectedIndexPath else { return nil }
+        guard let cell = self.tableView.cellForRow(at: selectedIndexPath) as? IssueListCell else { return nil }
+        
+        let sourceRect = self.tableView.convert(cell.frame, to: nil)
+        
+        self.issueDetailsAnimator.sourceView = cell.issueImageView
+        self.issueDetailsAnimator.sourceRect = sourceRect
+        self.issueDetailsAnimator.presenting = true
+        
+        return self.issueDetailsAnimator
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return nil
     }
 }
