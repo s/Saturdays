8aab45b22d53f1a231dda9b747fc1861eff23488
Said Ozcan <said.ozcan@booking.com>
2017-06-15 01:40:39 +0200

    seperate UIHelper and UIDefines

--- Saturdays/Common/Helpers/UIHelper.swift
+++ Saturdays/Common/Helpers/UIHelper.swift
@@ -9,58 +9,6 @@
 import UIKit
 import Shimmer
 
-struct UIDefines {
-    static let issueDetailsTransitionDuration : TimeInterval = 3
-    
-    fileprivate struct FontSizes {
-        static let small                            : CGFloat = 12.0
-        static let medium                           : CGFloat = 14.0
-        static let large                            : CGFloat = 16.0
-        static let xlarge                           : CGFloat = 18.0
-        static let xxxlarge                         : CGFloat = 22.0
-        static let navigationBarLargeTitleViewTitle : CGFloat = 34.0
-        static let navigationBarContentViewTitle    : CGFloat = 17.0
-    }
-    
-    struct Spacings {
-        static let singleUnit : CGFloat = 8.0
-        static let doubleUnit : CGFloat = 16.0
-        static let cellSpacing: CGFloat = 24.0
-    }
-    
-    struct Fonts {
-        static let title  = UIFont.systemFont(ofSize: UIDefines.FontSizes.large, weight: UIFontWeightRegular)
-        static let body   = UIFont.systemFont(ofSize: UIDefines.FontSizes.xlarge, weight: UIFontWeightRegular)
-        static let detail = UIFont.systemFont(ofSize: UIDefines.FontSizes.small, weight: UIFontWeightLight)
-        static let subtitle = UIFont.systemFont(ofSize: UIDefines.FontSizes.xxxlarge, weight: UIFontWeightBold)
-        static let navigationBarLargeTitleViewTitle = UIFont.systemFont(ofSize: UIDefines.FontSizes.navigationBarLargeTitleViewTitle,
-                                                                   weight:UIFontWeightBold)
-        static let navigationBarContentViewTitle = UIFont.systemFont(ofSize: UIDefines.FontSizes.navigationBarContentViewTitle,
-                                                                weight:UIFontWeightSemibold)
-    }
-    
-    struct Colors {
-        static let lightGray              = UIColor(red: 230.0/255.0, green: 230.0/255.0, blue: 230.0/255.0, alpha: 1.0)
-        static let white                  = UIColor.white
-        static let black                  = UIColor.black
-        static let navigationBarTintColor = UIDefines.Colors.white
-    }
-    
-    struct Copies {
-        static let saturdaysTitle = NSLocalizedString("SATURDAYS", comment: "")
-        static let tracksTitle = NSLocalizedString("Tracks", comment: "")
-    }
-    
-    struct Sizes {
-        static let defaultIssueCellHeight            : CGFloat = 250.0
-        static let navigationBarViewHeight           : CGFloat = 96.0
-        static let navigationBarLargeTitleViewHeight : CGFloat = 52.0
-        static let navigationBarContentViewHeight    : CGFloat = 44.0
-        static let issueListCellCornerRadius         : CGFloat = 8.0
-        static let issueDetailsDismissIconDimension  : CGFloat = 40.0
-    }
-}
-
 class UIHelper: NSObject {
     static func createShimmerBlocks(numberOf count:Int) -> UIStackView {
         let stackView = UIStackView()
