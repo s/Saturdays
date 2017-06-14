35eaec862ce2959fa4f4d0d80e107dcd85744327
Said Ozcan <said.ozcan@booking.com>
2017-06-15 01:41:10 +0200

    adds UILabelDescriptor

---
+++ Saturdays/Common/Helpers/UILabelDescriptor.swift
@@ -0,0 +1,15 @@
+//
+//  UILabelDescriptor.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 15/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import UIKit
+
+struct UILabelDescriptor {
+    let text : String
+    let font : UIFont
+    let textColor: UIColor
+}
