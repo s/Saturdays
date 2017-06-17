a5a52c1e02f82e993623573b7d4f63c1ecc38087
Said Ozcan <said.ozcan@booking.com>
2017-06-18 01:30:06 +0200

    add NotificationAction

---
+++ Saturdays/Common/Helpers/NotificationAction.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationAction.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 17/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationAction : String {
+    case openIssue
+    case dismiss
+    case tap = "com.apple.UNNotificationDefaultActionIdentifier"
+}
