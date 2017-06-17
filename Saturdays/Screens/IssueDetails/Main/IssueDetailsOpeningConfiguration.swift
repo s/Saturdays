602ce53b5aa15423848ee63222a8c0da4842e5b8
Said Ozcan <said.ozcan@booking.com>
2017-06-18 01:30:47 +0200

    add IssueDetailsOpeningConfiguration

---
+++ Saturdays/Screens/IssueDetails/Main/IssueDetailsOpeningConfiguration.swift
@@ -0,0 +1,15 @@
+//
+//  IssueDetailsOpeningConfiguration.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 17/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import UIKit
+
+struct IssueDetailsOpeningConfiguration {
+    let indexPath:IndexPath?
+    let item:IssueViewModel
+    let image:UIImage?
+}
