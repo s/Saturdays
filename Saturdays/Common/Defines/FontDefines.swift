3250c37a1c212f2f70d9acf2fe82415ac24c467e
Said Ozcan <said.ozcan@booking.com>
2017-06-25 15:53:40 +0200

    add new specific *Defines.swift files

---
+++ Saturdays/Common/Defines/FontDefines.swift
@@ -0,0 +1,13 @@
+//
+//  FontDefines.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 25/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+
+struct FontDefines {
+    
+}
---
+++ Saturdays/Common/Defines/SpacingDefines.swift
@@ -0,0 +1,13 @@
+//
+//  SpacingDefines.swift
+//  Saturdays
+//
+//  Created by Said Ozcan on 25/06/2017.
+//  Copyright © 2017 Said Ozcan. All rights reserved.
+//
+
+import Foundation
+
+struct SpacingDefines {
+    
+}
